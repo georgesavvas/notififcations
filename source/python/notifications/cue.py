6a085c12ea32bcad31d845ed65156e87eba48c03
George Savvas <george@electrictheatre.tv>
2024-06-09 23:44:38 +0100

    wip

--- source/python/notifications/cue.py
+++ source/python/notifications/cue.py
@@ -1,18 +1,23 @@
 import fnmatch
 import datetime
 from uuid import uuid4
+from pathlib import Path
 
+from . import slack
 from .tools import get_logger, get_collection, get_slackbot_collection
+
 
 
 LOGGER = get_logger(__name__)
 
 rules_coll = get_collection("notification_rules")
 farm_coll = get_slackbot_collection("store_farm")
+renders_coll = get_slackbot_collection("store_renders")
 renders_submitted_coll = get_collection("renders_submitted")
 renders_failing_coll = get_collection("renders_failing")
 renders_finished_coll = get_collection("renders_finished")
 messages_coll = get_collection("notification_messages")
+notification_times_coll = get_collection("notification_times")
 
 
 def is_rule_relevant(rule, job):
@@ -195,6 +200,21 @@ def process_finished(job):
         )
 
 
+def get_vri(job):
+    for layer in job.get("layers", []):
+        paths = layer.get("outputPaths", [])
+        for path in paths:
+            if not path or not path.startswith("/jobs"):
+                return ""
+            path = Path(path)
+            asset_dir = path.parent
+            vri_path = asset_dir / ".vri"
+            vri = ""
+            if vri_path.is_file():
+                return vri_path.read_text()
+    return ""
+
+
 def run():
     farm_data = farm_coll.find_one(sort=[("_id", -1)])
     if not farm_data:
@@ -204,3 +224,258 @@ def run():
         process_submitted(job)
         process_failing(job)
         process_finished(job)
+
+
+def run_summary():
+    # rules = rules_coll.find({"notified_for": "farm_summary"})
+    "theom, georgeg, alexga, yousef, tri"
+    rules = [{
+        "notified_for": "farm_summary",
+        "user": "dorianne",
+        "times": [0, 0]
+    }]
+    now = datetime.datetime.now()
+    yesterday_date = now - datetime.timedelta(days=3)
+    for rule in rules:
+        user = rule["user"]
+        times = rule.get("times")
+        if not times:
+            times = notification_times_coll.find_one({"user": user})
+        if not times:
+            LOGGER.error(f"No times found for user {user}, aborting...")
+            continue
+        specific_time = datetime.datetime.strptime("21:00", "%H:%M").time()
+        yesterday_specific_time = datetime.datetime.combine(yesterday_date, specific_time)
+        logoff_ts = yesterday_specific_time.timestamp()
+        print(user, "time:", yesterday_specific_time)
+        finished_renders = list(renders_coll.find({"user": user, "startTime": {"$gt": logoff_ts}}))
+        farm_data = farm_coll.find_one(sort=[("_id", -1)])
+        if not farm_data:
+            farm_data = []
+        running_renders = [render for render in farm_data["data"]["jobs"] if render["user"] == user and render["startTime"] > logoff_ts]
+        if not running_renders and not finished_renders:
+            LOGGER.warning(f"User {user} had no renders since {yesterday_specific_time}, skipping...")
+            continue
+        shows = {}
+        if running_renders and finished_renders:
+            for state, renders in [["running", running_renders], ["finished", finished_renders]]:
+                for render in renders:
+                    if not render.get("show") or not render.get("shot"):
+                        continue
+                    if render["shot"].startswith("none_"):
+                        continue
+                    if not shows.get(render["show"]):
+                        shows[render["show"]] = {"finished": [], "running": []}
+                    if render["shot"] in shows[render["show"]]["running"]:
+                        continue
+                    if render["shot"] in shows[render["show"]][state]:
+                        continue
+                    shows[render["show"]][state].append(render["shot"])
+        blocks = get_summary_blocks(finished_renders, running_renders, shows)
+        slack.send_message(
+            service="cue", text="Your Farm Summary", blocks=blocks, user="george"
+        )
+
+
+def get_running_time(job):
+        start = job["startTime"]
+        stop = job["stopTime"]
+        td = stop - start
+        return td
+
+
+def format_time(sec):
+        sec = int(sec)
+        hours = sec // 3600
+        mins = (sec % 3600) // 60
+        secs = (sec % 3600) % 60
+
+        if hours:
+            final = "{}h {}m {}s".format(hours, mins, secs)
+        elif mins:
+            final = "{}m {}s".format(mins, secs)
+        else:
+            final = "{}s".format(secs)
+        return final
+
+
+def get_shots_text(shots, show=None):
+    text = f"\n`{show}` shot" if show else "Shot"
+    finished = shots.get("finished")
+    running = shots.get("running")
+    finished_amount = len(finished)
+    running_amount = len(running)
+    if finished and running:
+        if finished_amount > 1:
+            text += "s "
+            text += ", ".join([f"`{shot}`" for shot in finished[:-1]]) + f" and `{finished[-1]}`"
+            text += " are done while "
+        else:
+            text += f" `{finished[0]}` is done while "
+        if running_amount > 1:
+            text += ", ".join([f"`{shot}`" for shot in running[:-1]]) + f" and `{running[-1]}` are still running."
+        else:
+            text += f"`{running[0]}` is still running."
+    elif finished:
+        if not show:
+            return "All shots are done."
+        if finished_amount > 1:
+            text += "shots "
+        text += "are all done."
+    elif running:
+        if not show:
+            return "All shots are still running."
+        if running_amount > 1:
+            text += "shots "
+        text += "are all still running."
+    print(text)
+    return text
+
+
+def get_render_cores(job):
+    cores = 0
+    for layer in job.get("layers", []):
+        cores += layer.get("currentCores", 0)
+    return cores
+
+
+def get_render_progress(job):
+    layers = job.get("layers", [])
+    total_percent = sum(layer.get("percentCompleted", 100) for layer in layers)
+    return round(total_percent / len(layers))
+
+
+def get_summary_blocks(finished, running, shows):
+    running_amount = len(running)
+    finished_amount = len(finished)
+    renders_amount = running_amount + finished_amount
+    finished_text = "all" if finished_amount == renders_amount else f"{finished_amount}"
+    renders_text = "render" if renders_amount == 1 else "renders"
+    shows_amount = len(shows)
+    shows_text = ""
+    if shows_amount == 1:
+        show = list(shows.keys())[0]
+        shows_text = " " + get_shots_text(shows[show])
+    elif shows_amount > 1:
+        shows_text = " " + "\n".join([get_shots_text(shots, show) for show, shots in shows.items()])
+    if renders_amount == 0:
+        return [{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": ":sunny: Morning! You had no renders on the farm last night.",
+        }
+    }]
+    else:
+        intro_text = f"You had {renders_amount} {renders_text} on the farm, {finished_text} of which are finished.{shows_text}"
+    finished_pattern = ">_{name}_\n>`{vri}`\n>Start-to-finish: {render_time}\n\n"
+    running_pattern = ">`{name}`\n>Progress: {progress}%\n>Current power: {cores_text}\n\n"
+    finished_text = ""
+    running_text = ""
+    for render in finished[:10]:
+        finished_text += finished_pattern.format(
+            name=render["name"],
+            vri=get_vri(render) or "Couldn't get VRI",
+            render_time=format_time(get_running_time(render)),
+        )
+    if finished_amount > 10:
+        finished_text += f"and {finished_amount - 10} more..."
+    for render in running[:10]:
+        cores_text = f"{get_render_cores(render)} cores"
+        running_text += running_pattern.format(
+            name=render["name"],
+            progress=get_render_progress(render),
+            cores_text=cores_text,
+        )
+    if running_amount > 10:
+        running_text += f"and {running_amount - 10} more..."
+    # spacer_block = {
+    #     "type": "section",
+    #     "text": {
+    #         "type": "plain_text",
+    #         "text": " ",
+    #     }
+    # }
+    spacer_block = {
+        "type": "divider"
+    }
+    # morning_block = {
+    #     "type": "section",
+    #     "text": {
+    #         "type": "mrkdwn",
+    #         "text": ":sunny: Morning! Here's your farm summary since yesterday",
+    #     },
+    #     "accessory": {
+    #         "type": "timepicker",
+    #         "initial_time": "21:00",
+    #         "placeholder": {
+    #             "type": "plain_text",
+    #             "text": "Select time",
+    #             "emoji": True
+    #         },
+    #         "action_id": "timepicker-action",
+    #     }
+    # }
+    morning_block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": ":sunny: Morning! Here's your farm summary since yesterday 21:00.",
+        }
+    }
+    intro_block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": intro_text,
+        }
+    }
+    finished_header_text = ":white_check_mark: Here's what's finished:"
+    if running_amount:
+        if finished_amount == 1:
+            finished_header_text = ":white_check_mark: Here it is:"
+        else:
+            finished_header_text = ":white_check_mark: Here they are:"
+    finished_header_block = {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": finished_header_text,
+			}
+		}
+    finished_block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": finished_text,
+        }
+    }
+    running_header_text = ":woman_in_lotus_position: Here's what you're waiting for:"
+    if finished_amount:
+        if running_amount == 1:
+            running_header_text = ":woman_in_lotus_position: The remaining one is:"
+        else:
+            running_header_text = ":woman_in_lotus_position: The remaining ones are:"
+    running_header_block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": running_header_text,
+        }
+    }
+    running_block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": running_text,
+        }
+    }
+    final = [
+        morning_block,
+        intro_block,
+    ]
+    if finished_text:
+        final += [spacer_block, finished_header_block, finished_block]
+    if running_text:
+        final += [spacer_block, running_header_block, running_block]
+    return final
--- source/python/notifications/main.py
+++ source/python/notifications/main.py
@@ -10,6 +10,17 @@ LOGGER = get_logger(__name__)
 messages_coll = get_collection("notification_messages")
 
 
+async def cue_summary():
+    while True:
+        start_time = time.time()
+        cue.run_summary()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        LOGGER.debug(f"Cue summary for {round(elapsed_time, 1)} seconds")
+        break
+        # await asyncio.sleep(60)
+
+
 async def cue_():
     while True:
         start_time = time.time()
@@ -21,12 +32,13 @@ async def cue_():
 
 
 async def volt_():
-    while True:
-        start_time = time.time()
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        LOGGER.debug(f"Volt run for {round(elapsed_time, 1)} seconds")
-        await asyncio.sleep(10)
+    pass
+    # while True:
+    #     start_time = time.time()
+    #     end_time = time.time()
+    #     elapsed_time = end_time - start_time
+    #     LOGGER.debug(f"Volt run for {round(elapsed_time, 1)} seconds")
+    #     await asyncio.sleep(10)
 
 
 def send_slack_message(msg):
@@ -42,17 +54,16 @@ send_functions = {
 
 
 async def main():
-    # await cue_()
-    # await volt_()
-    asyncio.gather(*[cue_(), volt_()])
-    while True:
-        LOGGER.debug("Sending slack messages")
-        messages = messages_coll.find()
-        for msg in messages:
-            success = send_functions[msg.get("delivery", "slack")](msg)
-            if success:
-                messages_coll.delete_one({"id": msg["id"]})
-        await asyncio.sleep(20)
+    # asyncio.gather(*[cue_(), volt_()])
+    asyncio.gather(*[cue_summary()])
+    # while True:
+    #     LOGGER.debug("Sending slack messages")
+    #     messages = messages_coll.find()
+    #     for msg in messages:
+    #         success = send_functions[msg.get("delivery", "slack")](msg)
+    #         if success:
+    #             messages_coll.delete_one({"id": msg["id"]})
+    #     await asyncio.sleep(20)
 
 
 asyncio.run(main())
